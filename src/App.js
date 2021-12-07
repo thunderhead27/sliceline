7e4d83143dace403f5d50f13d8e6da4a49ea622d
Jack Choi <thunderhead27@gmail.com>
2021-12-07 12:30:33 -0800

    separate globalstyle

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
+import { GlobalStyle } from "./styles/GlobalStyle";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Banner } from "./components/Banner/Banner";
 import Menu from "./components/Menu/Menu";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: 'Open Sans', sans-serif;
-  }
-
-  h1, h2, h3 {
-    font-family: 'Righteous', cursive;
-  }
-`;
 
 function App() {
   return (
